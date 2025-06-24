b4573d08e6e5e7d70bed283b0fbf226edf686067
MehmetAli <mehmetalikarakutuk02@gmail.com>
2025-06-24 20:47:47 +0300

    Update baglanti.php

--- baglanti.php
+++ baglanti.php
@@ -4,10 +4,10 @@ $username = "root";
 $password = ""; 
 $database = "bize_ulasin";
 
-// Bağlantı oluşturuluyor
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Bağlantı hatasını kontrol et
+
 if ($conn->connect_error) {
     die("Bağlantı hatası: " . $conn->connect_error);
 }
